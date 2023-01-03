6b18b78a3fc2fe880a015e2dc9f876268af1adbb
AechtRob <aecht_rob@outlook.com>
2023-01-03 15:51:54 +0000

    Initial 1.19 setup

--- src/main/java/com/aechtrob/prehistoricnature/items/ModItems.java
+++ src/main/java/com/aechtrob/prehistoricnature/items/ModItems.java
@@ -1,15 +1,19 @@
-package com.aechtrob.prehistoricnature.item;
+package com.aechtrob.prehistoricnature.items;
 
 import com.aechtrob.prehistoricnature.PrehistoricNatureMod;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, PrehistoricNatureMod.MOD_ID);
+
+    public static final RegistryObject<Item> FOSSIL_CLEAN = ITEMS.register("fossil_clean",
+            () -> new FossilCleanItem(new Item.Properties()));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);

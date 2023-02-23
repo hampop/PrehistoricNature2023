7fc8476a9dd4ad2d692d48aa11e7b8c2db2e2b8d
AechtRob <aecht_rob@outlook.com>
2023-02-23 15:42:34 +0000

    Lepidodendron tree and backfilling assets

---
+++ src/main/java/com/aechtrob/prehistoricnature/blocks/blocks/PNFenceGateFlammableBlock.java
@@ -0,0 +1,30 @@
+package com.aechtrob.prehistoricnature.blocks.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.FenceGateBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class PNFenceGateFlammableBlock extends FenceGateBlock {
+
+    public PNFenceGateFlammableBlock(Properties properties, SoundEvent soundClose, SoundEvent soundOpen) {
+        super(properties, soundClose, soundOpen);
+    }
+
+    @Override
+    public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return true;
+    }
+
+    @Override
+    public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 5;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 20;
+    }
+}
--- src/main/java/com/aechtrob/prehistoricnature/blocks/trees/lepidodendron/ModBlocksTreeLepidodendron.java
+++ src/main/java/com/aechtrob/prehistoricnature/blocks/trees/lepidodendron/ModBlocksTreeLepidodendron.java
@@ -1,20 +1,14 @@
 package com.aechtrob.prehistoricnature.blocks.trees.lepidodendron;
 
 import com.aechtrob.prehistoricnature.PrehistoricNatureMod;
-import com.aechtrob.prehistoricnature.blocks.blocks.PNRotatedPillarFlammableBlock;
-import com.aechtrob.prehistoricnature.world.tree.lepidodendron.LepidodendronTreeGrower;
+import com.aechtrob.prehistoricnature.blocks.blocks.*;
 import com.aechtrob.prehistoricnature.items.ModItems;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import com.aechtrob.prehistoricnature.world.tree.lepidodendron.LepidodendronTreeGrower;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.LeavesBlock;
-import net.minecraft.world.level.block.SaplingBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -63,46 +57,47 @@ public class ModBlocksTreeLepidodendron {
             () -> new PNRotatedPillarFlammableBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)
                     .requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> LEPIDODENDRON_LEAVES = registerBlock("lepidodendron_leaves",
-            () -> new LeavesBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LEAVES)
-                    .requiresCorrectToolForDrops()){
-                @Override
-                public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
-                    return true;
-                }
-
-                @Override
-                public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
-                    return 30;
-                }
-
-                @Override
-                public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
-                    return 60;
-                }
-            });
-    public static final RegistryObject<Block> LEPIDODENDRON_STROBILUS = registerBlock("lepidodendron_strobilus",
-            () -> new LepidodendronStrobilus(BlockBehaviour.Properties.copy(Blocks.GRASS)));
+            () -> new PNLeavesBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LEAVES)
+                    .requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> LEPIDODENDRON_SAPLING = registerBlock("lepidodendron_sapling",
             () -> new SaplingBlock(new LepidodendronTreeGrower(), BlockBehaviour.Properties.copy(Blocks.OAK_SAPLING)
                     .requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> LEPIDODENDRON_PLANKS = registerBlock("lepidodendron_planks",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)
-                    .requiresCorrectToolForDrops()){
-                @Override
-                public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
-                    return true;
-                }
+            () -> new PNPlanksFlammableBlock(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)
+                    .requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> LEPIDODENDRON_SLAB = registerBlock("lepidodendron_slab",
+            () -> new PNSlabFlammableBlock(BlockBehaviour.Properties.copy(Blocks.OAK_SLAB)
+                    .requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> LEPIDODENDRON_STAIRS = registerBlock("lepidodendron_stairs",
+            () -> new PNStairFlammableBlock(() -> ModBlocksTreeLepidodendron.LEPIDODENDRON_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS)
+                    .requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> LEPIDODENDRON_FENCE = registerBlock("lepidodendron_fence",
+            () -> new PNFenceFlammableBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE)
+                    .requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> LEPIDODENDRON_FENCE_GATE = registerBlock("lepidodendron_fence_gate",
+            () -> new PNFenceGateFlammableBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE_GATE)
+                    .requiresCorrectToolForDrops(), SoundEvents.FENCE_GATE_CLOSE, SoundEvents.FENCE_GATE_OPEN));
+    public static final RegistryObject<Block> LEPIDODENDRON_DOOR = registerBlock("lepidodendron_door",
+            () -> new DoorBlock(BlockBehaviour.Properties.copy(Blocks.OAK_DOOR)
+                    .requiresCorrectToolForDrops(), SoundEvents.WOODEN_DOOR_OPEN, SoundEvents.WOODEN_DOOR_OPEN));
+    public static final RegistryObject<Block> LEPIDODENDRON_TRAPDOOR = registerBlock("lepidodendron_trapdoor",
+            () -> new TrapDoorBlock(BlockBehaviour.Properties.copy(Blocks.OAK_DOOR)
+                    .requiresCorrectToolForDrops(), SoundEvents.WOODEN_TRAPDOOR_OPEN, SoundEvents.WOODEN_TRAPDOOR_OPEN));
+    public static final RegistryObject<Block> LEPIDODENDRON_BUTTON = registerBlock("lepidodendron_button",
+            () -> new ButtonBlock(BlockBehaviour.Properties.copy(Blocks.OAK_BUTTON)
+                    .requiresCorrectToolForDrops(), 30, true, SoundEvents.WOODEN_BUTTON_CLICK_OFF, SoundEvents.WOODEN_BUTTON_CLICK_ON));
+    public static final RegistryObject<Block> LEPIDODENDRON_BENCH = registerBlock("lepidodendron_bench",
+            () -> new ButtonBlock(BlockBehaviour.Properties.copy(Blocks.OAK_BUTTON)
+                    .requiresCorrectToolForDrops(), 30, true, SoundEvents.WOODEN_BUTTON_CLICK_OFF, SoundEvents.WOODEN_BUTTON_CLICK_ON));
 
-                @Override
-                public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
-                    return 5;
-                }
+    //How do signs work? There are a few different blocks for each one!
 
-                @Override
-                public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
-                    return 20;
-                }
-            });
+
+    //Other tree-specific stuff:
+    public static final RegistryObject<Block> LEPIDODENDRON_STROBILUS = registerBlock("lepidodendron_strobilus",
+            () -> new LepidodendronStrobilus(BlockBehaviour.Properties.copy(Blocks.GRASS)));
+
+
 
 }

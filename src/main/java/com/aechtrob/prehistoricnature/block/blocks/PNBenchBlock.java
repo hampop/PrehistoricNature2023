0681e57ddba5fc5e944ee6bd3fdce4c564d746af
AechtRob <Aecht_Rob@outlook.com>
2023-02-28 12:22:23 +0000

    Fixed trapdoor model

--- src/main/java/com/aechtrob/prehistoricnature/block/blocks/PNBenchBlock.java
+++ src/main/java/com/aechtrob/prehistoricnature/block/blocks/PNBenchBlock.java
@@ -1,15 +1,19 @@
 package com.aechtrob.prehistoricnature.block.blocks;
 
-import com.aechtrob.prehistoricnature.entity.block.*;
-import net.minecraft.core.*;
-import net.minecraft.world.*;
-import net.minecraft.world.entity.player.*;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.entity.*;
-import net.minecraft.world.level.block.state.*;
-import net.minecraft.world.phys.*;
-import org.jetbrains.annotations.*;
+import com.aechtrob.prehistoricnature.entity.block.blocks.PNSignBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 
 public class PNBenchBlock extends BaseEntityBlock {
     protected PNBenchBlock(Properties p_49224_) {
